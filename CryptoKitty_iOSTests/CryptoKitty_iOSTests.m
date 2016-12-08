266f146f848bbe07693228cf18dbace91248da0d
Steve Brenneis <swb102856@gmail.com>
2016-12-07 20:03:43 -0500

    Initial Commit

---
+++ CryptoKitty_iOSTests/CryptoKitty_iOSTests.m
@@ -0,0 +1,39 @@
+//
+//  CryptoKitty_iOSTests.m
+//  CryptoKitty_iOSTests
+//
+//  Created by Steve Brenneis on 12/7/16.
+//  Copyright © 2016 CryptoKitty. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CryptoKitty_iOSTests : XCTestCase
+
+@end
+
+@implementation CryptoKitty_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
